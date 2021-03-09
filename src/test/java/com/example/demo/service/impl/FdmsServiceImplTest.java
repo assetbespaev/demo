89a62d7d2d2aa8b6dc3143a97f9697fbc0603fca
abespayev <abespayev@beeline.kz>
2021-03-09 09:02:54 +0600

    Test #2

--- src/test/java/com/example/demo/service/impl/FdmsServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/FdmsServiceImplTest.java
@@ -3,6 +3,7 @@ package com.example.demo.service.impl;
 import com.example.demo.wsdl.ChangePasswordByLoginResult;
 import com.example.demo.wsdl.ChangePasswordByLoginResultResponse;
 import com.example.demo.wsdl.ChangePasswordByLoginResultStruct;
+import com.example.demo.wsdl.ObjectFactory;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -21,8 +22,12 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.util.StreamReaderDelegate;
+import javax.xml.transform.stream.StreamSource;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLFilterImpl;
 
 class FdmsServiceImplTest {
 
@@ -134,11 +139,12 @@ class FdmsServiceImplTest {
   @Test
   void response_should_be_one() throws FileNotFoundException, XMLStreamException, JAXBException {
     XMLInputFactory xif = XMLInputFactory.newFactory();
-    XMLStreamReader xsr = xif.createXMLStreamReader(new FileReader("src/forum11465653/response_should_be.xml"));
-    xsr.nextTag(); // Advance to Envelope tag
+    XMLStreamReader xsr = xif
+        .createXMLStreamReader(new FileReader("src/forum11465653/response_should_be.xml"));
+    /*xsr.nextTag(); // Advance to Envelope tag
     xsr.nextTag(); // Advance to Body tag
     xsr.nextTag(); // Advance to getNumberResponse tag
-    System.out.println(xsr.getNamespaceContext().getNamespaceURI("ns1"));
+    System.out.println(xsr.getNamespaceContext().getNamespaceURI("ns1"));*/
 
     JAXBContext jc = JAXBContext.newInstance(ChangePasswordByLoginResultResponse.class);
     Unmarshaller unmarshaller = jc.createUnmarshaller();
@@ -148,6 +154,20 @@ class FdmsServiceImplTest {
     System.out.println(je.getName());
     System.out.print("value: ");
     System.out.println(je.getValue());
+  }
+
+  @Test
+  void test() throws FileNotFoundException, XMLStreamException, JAXBException {
+    JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
+    XMLInputFactory xif = XMLInputFactory.newFactory();
+    xif.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false); // this is the magic line
+    StreamSource source = new StreamSource(
+        new FileInputStream("src/forum11465653/response_should_be.xml"));
+    XMLStreamReader xsr = xif.createXMLStreamReader(source);
+    Unmarshaller unmarshaller = jc.createUnmarshaller();
+    Object response =  unmarshaller
+        .unmarshal(xsr);
+    System.out.println(response);
   }
 
   @Test
@@ -166,18 +186,16 @@ class FdmsServiceImplTest {
 
     marshaller.marshal(response, new PrintWriter(System.out));
   }
-
 }
 
-class XXHeaderXMLReader
-    extends StreamReaderDelegate {
+class XMLReaderWithoutNamespace extends StreamReaderDelegate {
 
-  public XXHeaderXMLReader(XMLStreamReader paramXMLStreamReader) {
-    super(paramXMLStreamReader);
+  public XMLReaderWithoutNamespace(XMLStreamReader reader) {
+    super(reader);
   }
 
   @Override
-  public String getAttributeNamespace(int paramInt) {
+  public String getAttributeNamespace(int arg0) {
     return "";
   }
 
@@ -185,4 +203,72 @@ class XXHeaderXMLReader
   public String getNamespaceURI() {
     return "";
   }
+}
+
+class NamespaceFilter extends XMLFilterImpl {
+
+  private String usedNamespaceUri;
+  private boolean addNamespace;
+
+  //State variable
+  private boolean addedNamespace = false;
+
+  public NamespaceFilter(String namespaceUri,
+      boolean addNamespace) {
+    super();
+
+    if (addNamespace) {
+      this.usedNamespaceUri = namespaceUri;
+    } else {
+      this.usedNamespaceUri = "";
+    }
+    this.addNamespace = addNamespace;
+  }
+
+
+  @Override
+  public void startDocument() throws SAXException {
+    super.startDocument();
+    if (addNamespace) {
+      startControlledPrefixMapping();
+    }
+  }
+
+
+  @Override
+  public void startElement(String uri, String localName, String qName, Attributes atts)
+      throws SAXException {
+    super.startElement(this.usedNamespaceUri, localName, qName, atts);
+  }
+
+  @Override
+  public void endElement(String arg0, String arg1, String arg2)
+      throws SAXException {
+
+    super.endElement(this.usedNamespaceUri, arg1, arg2);
+  }
+
+  @Override
+  public void startPrefixMapping(String prefix, String url)
+      throws SAXException {
+
+    if (addNamespace) {
+      this.startControlledPrefixMapping();
+    } else {
+      //Remove the namespace, i.e. don´t call startPrefixMapping for parent!
+    }
+
+  }
+
+  private void startControlledPrefixMapping() throws SAXException {
+
+    if (this.addNamespace && !this.addedNamespace) {
+      //We should add namespace since it is set and has not yet been done.
+      super.startPrefixMapping("", this.usedNamespaceUri);
+
+      //Make sure we dont do it twice
+      this.addedNamespace = true;
+    }
+  }
+
 }
